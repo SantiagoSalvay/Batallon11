d58e6fe9cc100e178886c08042ce981832eea25c
Santiago Salvay <juansantiagosalvaymendez@gmail.com>
2026-05-16 00:05:10 -0300

    feat: initialize Prisma database schema and configure Vite development proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
+      host: true,
       port: 5173,
       proxy: {
         '/api': { target: apiUrl, changeOrigin: true },
